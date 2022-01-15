f971c63d17f6c2607374f9aa200604389e808021
milkiyd <daniel.attitsogbe02@gmail.com>
2022-01-15 21:11:21 +0000

    add theme colors

---
+++ src/styles/themes.js
@@ -0,0 +1,12 @@
+
+export const lightTheme = {
+    body: "hsl(0, 0%, 98%)",
+    fontColor: "hsl(200, 15%, 8%)",
+    componentBackground: "hsl(0, 0%, 90%)",
+}
+
+export const darkTheme = {
+    body: "hsl(207, 26%, 17%)",
+    fontColor: "hsl(0, 0%, 100%)",
+    componentBackground: "hsl(209, 23%, 22%)",
+}
